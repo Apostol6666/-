3dfec6bc2972e658c2c6d4851ea77f7bda48a423
Apostol6666 <44125687+Apostol6666@users.noreply.github.com>
2020-05-23 23:12:03 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>New Site</title>
+    <link href="style.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:700&display=swap" rel="stylesheet">
+     <! -- <script src="script.js"></script> 
+</head>
+  
+<body>
+  	<button class="buttonmus" type="button" onclick="new Audio('old_road.mp3').play()"> </button>
+    <p class="name"> <strong class="light"> TABLE </strong> </p>
+    <br> <br>
+  		<? include ('table.php'); ?>
+  	<br>
+  		<? include ('phrase.php'); ?>
+  	<br>
+  		<? include ('forma.php'); ?>
+    <br>
+  	<br>
+  		<? include ('admin.php'); ?>
+    <br>
+    <br>
+ 	 		<? include ('form2.php'); ?>
+  	<br>
+  		<? include ('admin2.php'); ?>
+  	<br>
+		<a href ="mypic.jpg"> Picture Link </a>
+  	<br>
+  	<a href ="mycite.html"> Site Link </a>
+  	<br>
+    <p>
+    <img src="smile.png" alt=":)">
+    <img src="wink.png" alt=";)">
+		<img src="sad.png" alt=":(">
+		</p>
+  	<img src="cat.png">
+  	<img src="/home/kate.hostfl.ru/public_html/mus.png" alt="heyhey">
+  	<br>
+    	<? include ('regular.php'); ?>
+  	<br>
+    <img class="cat" src="cat.png">
+</body>
+  
+</html>
